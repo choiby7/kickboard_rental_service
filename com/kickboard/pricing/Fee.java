776d040be93aee9131f469a86bfc1c0e2f3b5b3d
kim030311 <steven3407115@dankook.ac.kr>
2025-10-07 13:35:08 +0900

    Update Fee.java

--- com/kickboard/pricing/Fee.java
+++ com/kickboard/pricing/Fee.java
@@ -1,2 +1,16 @@
 package pricing;
 
+import java.math.BigDecimal;
+
+/**
+ * Fee.java			: 할인, 추가 요금 등이 모두 반영된최종 결제 금액을 반환
+ * @author			: Mingwan Kim
+ * @email			: steven3407115@dankook.ac.kr
+ * @version			: 1.0
+ * @date			: 2025.10.07
+ */
+public interface Fee {
+
+	//최종 결제 금액을 반환
+    BigDecimal getFinalCost();
+}
