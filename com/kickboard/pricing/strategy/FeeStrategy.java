a8702bd170b43a1e7d7952402ab85758d5cd6053
kim030311 <steven3407115@dankook.ac.kr>
2025-10-07 13:36:31 +0900

    Update FeeStrategy.java

--- com/kickboard/pricing/strategy/FeeStrategy.java
+++ com/kickboard/pricing/strategy/FeeStrategy.java
@@ -1,2 +1,25 @@
 package pricing.strategy;
 
+import java.math.BigDecimal;
+
+/**
+ * FeeStrategy.java	: 요금 계산 전략 인터페이스 - 시간 기반, 거리 기반 방식을 정의한다.
+ * @author	: Mingwan Kim
+ * @email	: steven3407115@dankook.ac.kr
+ * @version	: 1.0
+ * @date	: 2025.10.07
+ */
+public interface FeeStrategy {
+
+    /**
+     * 요금 계산 메서드
+     * @return 계산된 요금 (BigDecimal)
+     */
+    BigDecimal calculateFee();
+
+    /**
+     * 전략 이름 반환 ("Time-based", "Distance-based")
+     * @return 전략 이름 (String)
+     */
+    String name();
+}
