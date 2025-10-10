f21ca9e824990675ce422a7799bbac4f4e7fa306
minsk551 <minsk05151@gmail.com>
2025-10-11 00:01:01 +0900

    feat(RentalInfo): RentalInfo 초기 구현

--- com/kickboard/domain/rental/RentalInfo.java
+++ com/kickboard/domain/rental/RentalInfo.java
@@ -4,6 +4,14 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 public final class RentalInfo {
+
+    /**
+    * RentalInfo.java	: RentalInfo 초기 구현
+    * @author	: Minsu Kim
+    * @email	: minsk05151@gmail.com
+    * @version	: 1.0
+    * @date	: 2025.10.10
+    */
     private final LocalDateTime startTime;
     private final LocalDateTime endTime;       
     private final double traveledDistance;
@@ -11,7 +19,7 @@ public final class RentalInfo {
     public RentalInfo(LocalDateTime startTime, LocalDateTime endTime, double traveledDistance) {
         this.startTime = Objects.requireNonNull(startTime, "startTime");
         this.endTime = endTime;
-        if (traveledDistanceKm < 0) throw new IllegalArgumentException("distance < 0");
+        if (traveledDistanceKm < 0) throw new IllegalArgumentException("distance < 0"); // distance는 항상 0 이상
             this.traveledDistanceKm = traveledDistanceKm;
     }
 }
