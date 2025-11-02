4315b3b782f62426745ae3e49a44a2d39d00b4dd
leopard7777 <leopard7267@gmail.com>
2025-11-02 19:46:05 +0900

    refactor : 중복 메서드 리팩터링

--- com/kickboard/domain/user/DriverLicense.java
+++ com/kickboard/domain/user/DriverLicense.java
@@ -16,18 +16,12 @@ public class DriverLicense {
     }
 
     /**
-     * 면허증 유효성 검사
-     */
-    public void validate() {
-        if (licenseNumber.length() == 12) valid = true;
-        else valid = false;
-    }
-
-    /**
      * 유효성 여부 반환
      * @return valid
      */
-    public boolean isVaild() {
+    public boolean isValid() {
+        if (licenseNumber.length() == 12) valid = true;
+        else valid = false;
         return valid;
     }
 }
