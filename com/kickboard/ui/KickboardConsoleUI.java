347c808f1f27cf75be788bb5bcd21de9900f1daf
minsk551 <minsk05151@gmail.com>
2025-11-29 15:39:33 +0900

    Update KickboardConsoleUI: 2. 할인 선택 ~ 4. 결제수단 선택 로직 수정 (수정#2)

--- com/kickboard/ui/KickboardConsoleUI.java
+++ com/kickboard/ui/KickboardConsoleUI.java
@@ -17,6 +17,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
